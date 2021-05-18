611537a36b2a2da1573b374bb6a84ea678a31241
Luke Gill <bagrad007@gmail.com>
2021-05-18 14:59:20 -0400

    Done.

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -5,7 +5,7 @@ export default class GifList extends Component {
     render() {
         return (
             <div>
-                {props.gifs.map(gif => <img key={gif.url} src={gif.url} alt="gif" />)}
+                {this.props.gifs.map(gif => <img key={gif.url} src={gif.url} alt="gif" />)}
             </div>
         )
     }
